792692d281795cea66eb7e217e3d36ff2e4df05e
HE7086 <me@heyi7086.com>
2024-07-26 17:37:40 +0200

    Add support for bsd

--- lua/sudoedit/init.lua
+++ lua/sudoedit/init.lua
@@ -3,6 +3,9 @@ local M = {}
 M.parent = false
 M.cmdline = {}
 
+local is_linux = vim.fn.has("linux") == 1
+local is_bsd = vim.fn.has("bsd") == 1
+
 local function slice(arr, start, end_pos)
   local pos, new = 1, {}
   for i = start, end_pos do
@@ -16,13 +19,22 @@ function M.get_proc_status(pid)
   if pid then
     return vim.fn.readfile(string.format("/proc/%i/status", pid))
   else
-    return vim.fn.readfile("/proc/self/status")
+    if is_linux then
+      return vim.fn.readfile("/proc/self/status")
+    elseif is_bsd then
+      return vim.fn.readfile("/proc/curproc/status")
+    end
   end
 end
 
 function M.get_ppid(pid)
   local status = M.get_proc_status(pid)
-  return vim.fn.matchlist(status, [[^PPid:\s\+\(\d\+\)]])[2]
+
+  if is_linux then
+    return vim.fn.matchlist(status, [[^PPid:\s\+\(\d\+\)]])[2]
+  elseif is_bsd then
+    return vim.fn.split(status[1], " ")[3]
+  end
 end
 
 function M.get_cmdline(pid)
@@ -51,7 +63,7 @@ function M.is_sudoedit()
 end
 
 function M.detect()
-  if M.is_sudoedit() then
+  if (is_linux or is_bsd) and M.is_sudoedit() then
 
     for _, filename in pairs(M.cmdline) do
       local buf = vim.api.nvim_get_current_buf()
